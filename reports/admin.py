3f50340f98c75038a03a7db01a2567ce70b4023d
ismaeel muhammad <ismaeelmuhammad123@gmail.com>
2023-02-03 12:27:43 +0100

    git ignore

--- reports/admin.py
+++ reports/admin.py
@@ -3,7 +3,7 @@ from .models import Report, Ward, PollingUnit, AgentProfile
 
 
 @admin.register(Ward)
-class ReportAdmin(admin.ModelAdmin):
+class WardAdmin(admin.ModelAdmin):
     list_display = [
         "local_government",
         "name",
@@ -22,9 +22,34 @@ class PollingUnitAdmin(admin.ModelAdmin):
 
 @admin.register(AgentProfile)
 class AgentProfileAdmin(admin.ModelAdmin):
-    pass
+    def full_name(self, obj):
+        return obj.user.get_full_name()
+
+    def email(self, obj):
+        return obj.user.email
+
+
+    list_display = [
+        "full_name",
+        "email",
+        "phone_number",
+        "address",
+        "assigned_ward",
+    ]
 
 
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
-    pass
+    def ward(self, obj):
+        return obj.polling_unit.ward
+    list_display = [
+        "agent",
+        "ward",
+        "polling_unit",
+        "uploaded_at",
+        "recorded_at",
+        "pdp",
+        "apc",
+        "nnpp",
+        "image",
+    ]
